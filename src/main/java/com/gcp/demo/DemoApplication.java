27f2e4150015f9dc2f487f128fdd081284424d7e
Christian Chunga <cchunga@safike.com>
2021-03-17 08:12:34 -0500

    TASK-2: Calcular área del cuadrado

--- src/main/java/com/gcp/demo/DemoApplication.java
+++ src/main/java/com/gcp/demo/DemoApplication.java
@@ -10,6 +10,9 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    public static Double calculateSquareArea(Double side) {
+        return (side*side);
+    }
     private static Double calcularTriangulo(Double base,Double altura){
         return (base*altura)/2;
     }
